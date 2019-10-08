79c4646bbb1d835a98a111cf6de515240d42db85
yoon <yuueuni@gmail.com>
2019-10-08 18:31:07 +0900

    2019-10-08 update3

--- bigday/views.py
+++ bigday/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'bigday/index.html')
+    return render(request, 'bigday/sample.html')
