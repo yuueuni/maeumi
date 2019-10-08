ee05758de7b15a40e0c90bbafb102fbc5138dd07
yoon <yuueuni@gmail.com>
2019-10-08 10:51:02 +0900

    2019-10-08 update1

--- bigday/views.py
+++ bigday/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponse
+from django.template import loader
+from .models import Member
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("check")
+    return render(request, 'bigday/index.html')
