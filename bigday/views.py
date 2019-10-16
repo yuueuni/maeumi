ba55f5671454deef0b6b95291184d621899bf9dd
yoon <yuueuni@gmail.com>
2019-10-16 16:28:29 +0900

    2019-10-16 DB test

--- bigday/views.py
+++ bigday/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from .forms import RegisterForm
 
 
 # Create your views here.
@@ -14,3 +15,17 @@ def test(request):
 
 def report(request):
     return render(request, 'bigday/finalReport.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'bigday/test.html', {'new_user':new_user})
+        else:
+            user_form = RegisterForm()
+
+        return render(request, 'bigday/test.html', {'form':user_form})
--- config/settings.py
+++ config/settings.py
@@ -15,18 +15,17 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@dk%1-f+9!=14u1abjff)e3)rl+t+w)l%ibsar=$my7u0gqjc0'
+with open('C:\\Users\\acorn\\Desktop\\secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.198','localhost','127.0.0.1']
-
+ALLOWED_HOSTS = ['localhost','192.168.0.198','127.0.0.1']
 
 # Application definition
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -84,7 +83,6 @@ DATABASES = {
         'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -118,9 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = '/'
